b45d6b21074fbc608cda647cdefb5ff65f648ec5
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-07-21 16:31:02 +0530

    Binary Search - LN
    *****Link******
    https://leetcode.com/problems/binary-search/
    ******Description******
    
    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ Binary-Search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int size = nums.size();
+        int start = 0;
+        int end = size-1;
+        int mid = (start+end)/2;
+
+        while(start<=end){
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(target> nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end =  mid-1;
+            }
+
+            mid = (start+end)/2;
+        }
+        
+        return -1;
+    }
+};
