e268f7372b0561fbcb17cb96a309b719ba817f14
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-08-01 23:32:48 +0530

    Remove All Occurrences of a Substring - LC
    *****Link*****
    https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
    *****Description******
    Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
    
    Find the leftmost occurrence of the substring part and remove it from s.
    Return s after removing all occurrences of part.
    A substring is a contiguous sequence of characters in a string.

---
+++ RemoveOccurrencesSubstring.cpp
@@ -0,0 +1,39 @@
+string replaceSpaces(string &str){
+
+ int n=str.length();
+
+    int count = 0;
+
+   // we will store the count of how many spaces are there
+
+   for(int i=0;i<n;i++){
+
+       if(str[i]==' '){
+
+           count++;
+
+       }
+
+   }
+
+   // below we have increase the size of string in order to iterate completely
+
+   // as when we add @40 in place of space the size of string increases
+
+   n=str.length() + (count*2);  
+
+   for(int i=0;i<n;i++){
+
+       if(str[i]==' '){
+
+           str.erase(i,1);
+
+           str.insert(i,"@40");
+
+       }
+
+   }
+
+   return str;
+
+}
