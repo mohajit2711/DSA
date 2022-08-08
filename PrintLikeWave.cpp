158cda935823a04f9b8975a2a8f86ea979817fcd
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-08-08 19:00:33 +0530

    Print Like A Wave - CN
    ***Link****
    https://www.codingninjas.com/codestudio/problems/print-like-a-wave_893268?utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_6&leftPanelTab=0

---
+++ PrintLikeWave.cpp
@@ -0,0 +1,24 @@
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector<int> ans;
+    for(int col = 0; col<mCols;col++){
+        if(col&1){
+            //odd Index -> Bottom to top
+            for(int row = nRows-1; row>=0;row--){
+//                 cout<< arr[row][col]<<" ";
+                ans.push_back(arr[row][col]);
+            }
+        }
+        else{
+            //even Index -> Top to Bottom
+            
+             for(int row = 0; row<nRows;row++){
+//                 cout<< arr[row][col]<<" ";
+                 ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    
+    return ans;
+}
